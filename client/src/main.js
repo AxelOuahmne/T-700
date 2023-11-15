28031be90740537b83101360d33060d754a50715
nathanibgui <77054517+nathanibgui@users.noreply.github.com>
2023-11-15 14:07:48 +0100

    test

--- client/src/main.js
+++ client/src/main.js
@@ -9,16 +9,19 @@ import 'boxicons/css/boxicons.min.css';
 
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+/*import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/*import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+/*import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret)
+/*library.add(faUserSecret)*/
 
 
- createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+/*createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');*/
+
+createApp(App).use(router).mount('#app');
+
